9948ab8ba0d7f08ef13ae8819eaa5cc578e2fde7
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-07-30 19:17:28 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,22 @@
-const CACHE = 'rayliziie-v1';
-const FILES = [
+const CACHE = 'rayliziie-v2';
+const ASSETS = [
   '/',
   '/index.html',
-  '/css/style.css',
-  '/js/app.js',
   '/manifest.json'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
