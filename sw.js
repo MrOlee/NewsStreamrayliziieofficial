232db64853dc686f37fd340f0412ccc397eaea5f
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-07-30 21:36:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'raylizii-cache-v1';
+const CACHE_NAME = 'raylizii-v2';
 const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', event => {
